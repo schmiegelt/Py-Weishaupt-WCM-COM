fbbe42521cf29fc148e6fd3f581ce6bec1a9f965
Philip <philip@schmiegelt.name>
2019-09-11 22:59:50 +0200

    adding dependency on 'requests'

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weishaupt-wcm-com",
-    version="0.0.1",
+    version="0.0.5",
     author="Philip Schmiegelt",
     author_email="philip@schmiegelt.it",
     description="Interfacing the Weishaupt WCM-COM module",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/schmiegelt/Py-Weishaupt-WCM-COM",
+    install_requires=["requests"],
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
 )

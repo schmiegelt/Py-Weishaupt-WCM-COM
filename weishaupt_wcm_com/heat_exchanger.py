c251f94e5db174e1ae2d3874d9d863719212b65c
Philip <philip@schmiegelt.name>
2019-09-16 22:38:55 +0200

    Change query to only retrieve oil consumption for now

--- weishaupt_wcm_com/heat_exchanger.py
+++ weishaupt_wcm_com/heat_exchanger.py
@@ -3,7 +3,7 @@ import json
 from requests.auth import HTTPDigestAuth
 
 ENDPOINT = "/parameter.json"
-QUERYTELEGRAM = '{"prot":"coco","telegramm":[[10,0,1,3196,0,0,0,0],[10,0,1,3197,0,0,0,0],[10,0,1,3198,0,0,0,0],[10,0,1,3199,0,0,0,0],[10,0,1,700,0,0,0,0],[10,0,1,3793,0,0,0,0],[10,0,1,3792,0,0,0,0],[0,0,1,5016,255,1,0,0],[0,0,1,5009,255,1,0,0]]}'
+QUERYTELEGRAM = '{"prot":"coco","telegramm":[[10,0,1,3793,0,0,0,0]]}'
 
 
 OILCONSUMTION_ID = 3793

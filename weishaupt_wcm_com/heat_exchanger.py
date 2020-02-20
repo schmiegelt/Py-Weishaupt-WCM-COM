4f71654e7ab1a525b0991ba08492bf17e26e1866
Philip <philip@schmiegelt.name>
2020-02-20 21:55:17 +0100

    fix for oil meter > 1000, print exception

--- weishaupt_wcm_com/heat_exchanger.py
+++ weishaupt_wcm_com/heat_exchanger.py
@@ -1,17 +1,21 @@
 import requests
 import json
+import sys
 from requests.auth import HTTPDigestAuth
+
 
 ENDPOINT = "/parameter.json"
 QUERYTELEGRAM = (
-    '{"prot":"coco","telegramm":[[10,0,1,3793,0,0,0,0],[10,0,1,3792,0,0,0,0],[10,0,1,12,0,0,0,0],[10,0,1,14,0,0,0,0],[10,0,1,3101,0,0,0,0],[10,0,1,325,0,0,0,0],[10,0,1,3197,0,0,0,0]]}'
+    '{"prot":"coco","telegramm":[[10,0,1,4176,0,0,0,0],[10,0,1,3793,0,0,0,0],[10,0,1,3792,0,0,0,0],[10,0,1,12,0,0,0,0],[10,0,1,14,0,0,0,0],[10,0,1,3101,0,0,0,0],[10,0,1,325,0,0,0,0],[10,0,1,3197,0,0,0,0]]}'
 )
 
+##[10,0,1,4176,0,0,0,0],
 VALUE = 1
 TEMP = 2
+DECIMAL_VALUE = 3
 
 # ID, Name, Value/Temp
-QUERIES = [[3793, "Oil Meter", VALUE], [3197, "Load Setting", VALUE], [12, "Outside Temperature", TEMP], [14, "Warm Water Temperature", TEMP], [3101, "Flow Temperature", TEMP], [325, "Flue Gas Temperature", TEMP]]
+QUERIES = [[3793, "Oil Meter", VALUE], [4176, "Load Setting", DECIMAL_VALUE], [12, "Outside Temperature", TEMP], [14, "Warm Water Temperature", TEMP], [3101, "Flow Temperature", TEMP], [325, "Flue Gas Temperature", TEMP]]
 
 
 
@@ -21,6 +25,9 @@ def getTemperture(lowByte, highByte):
 
 def getValue(lowByte, highByte):
     return lowByte + 265 * highByte
+
+def getDecimalValue(lowByte, highByte):
+    return (lowByte + 265 * highByte) / 10
 
 
 def process_values(server, username, password):
@@ -38,13 +45,15 @@ def process_values(server, username, password):
                 if message[3] == reading[0]:
                     if reading[2] == TEMP:
                         result[reading[1]] = getTemperture(message[6], message[7])
-                    else:
+                    elif reading[2] == VALUE:
                         result[reading[1]] = getValue(message[6], message[7])
+                    elif reading[2] == DECIMAL_VALUE:
+                        result[reading[1]] = getDecimalValue(message[6], message[7])
             # special handling for oil meter
             if message[3] == 3792:
                 result["Oil Meter"] = result["Oil Meter"]+message[6]*1000
         return json.dumps(result)
     except:
-        print("Error getting readings")
+        print ("Unexpected error:", sys.exc_info()[0])
         return None
 
